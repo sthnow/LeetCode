5a2d57943b2e04d6b1a206f6c20e6a9c39d47eaf
wzz <wangzh.work@outlook.com>
2018-03-11 15:30:03 +0800

    完成了所有功能

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -20,36 +20,51 @@ public class ReverseInteger {
         return s;
     }
 
+
     public int[] reverse(String s) {
         //先将字符串转换为String数组
         String[] ss = s.split("");
-        //定义一个int数组
-        int[] num = new int[ss.length];
-        int[] temp = num;
-        int number = 0;
 
 
-        //  if(ss[0] == "+" || ss[0] == "-"){
-        //
-        //  }
-        //将String数组转换为int数组
-        for (int i = 0; i <= ss.length - 1; i++) {
-            num[i] = Integer.parseInt(ss[i]);
+        //字符之间的比较不能用 "==",这个是比引用
+        //带有字符的情况
+        if (ss[0].equals("+") || ss[0].equals("-")) {
+            System.out.print(ss[0]);
+
+            int[] num = new int[ss.length - 1];
+            int[] temp = num;
+            int number = 0;
+
+            //将String数组转换为int数组
+            for (int i = 1; i <= ss.length - 1; i++) {
+                num[i - 1] = Integer.parseInt(ss[i]);
+            }
+
+            ReverseInteger ri = new ReverseInteger();
+            return temp = ri.normalReverse(num);
+
+
+        } else {
+            //不带有字符的情况
+            int[] num = new int[ss.length];
+            int[] temp = num;
+            int number = 0;
+
+
+            //将String数组转换为int数组
+            for (int i = 0; i <= ss.length - 1; i++) {
+                num[i] = Integer.parseInt(ss[i]);
+            }
+            ReverseInteger ri = new ReverseInteger();
+            return temp = ri.normalReverse(num);
         }
+    }
 
-        /**
-         * reverse分三种类型
-         *      1.123这种
-         *      2.+/-123这种
-         *      3.120这种
-         *
-         *      对于第一种，直接reverse即可
-         *      第二种，判断末尾有没有0
-         *      第三种，判断首位有没有正负号
-         *
-         */
 
-        //对于第一种和第二种情况
+
+    //反转的方法,由reverse调用
+    public int[] normalReverse(int[] num) {
+        int number = 0;
         for (int i = num.length - 1; i >= 0; i--) {
             if (num[i] == 0) {
                 number = number + 1;
@@ -57,15 +72,15 @@ public class ReverseInteger {
                 break;
             }
         }
-
         int[] end = new int[num.length - number];
         for (int j = num.length - 1 - number; j >= 0; j--) {
             end[num.length - 1 - number - j] = num[j];
-            //        System.out.print(end[num.length - 1 - number - j]);
         }
-
         return end;
-
     }
 }
+
+
+
+
 
