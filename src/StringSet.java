3f057138868c57715b2b0d37cd3ab7c9685069ed
wzz <568780720@qq.com>
2018-08-06 23:04:35 +0800

    leetcode  ·颠倒数组

--- src/StringSet.java
+++ src/StringSet.java
@@ -1,3 +1,20 @@
 public class StringSet {
+    public void reverseString(String s) {
+        //定义变量
+        String rs = "";
+        StringBuffer sb = new StringBuffer();
+        String[] array = s.split("");
+        String[] reArray = new String[array.length];
 
+        //颠倒字符串
+        for (int i = 0; i < array.length; i++) {
+            reArray[i] = array[array.length - i];
+            sb.append(reArray[i]);
+        }
+        rs = sb.toString();
+        //输出
+        System.out.println(rs);
+
+    }
 }
+
