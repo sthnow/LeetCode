627f4b999649d43ff21d242da908ff5dc2e7ee37
wzz <568780720@qq.com>
2018-08-13 15:27:04 +0800

    leetcode  ·有效的字母异位词

--- src/StringSet.java
+++ src/StringSet.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringSet {
     public void reverseString(String s) {
         //定义变量
@@ -78,9 +80,8 @@ public class StringSet {
 
     public int strStr(String haystack, String needle) {
         String[] str = haystack.split(needle);
-        int length = str.length;
-        if (str.length == haystack.length()) {
-            if (needle.equals("")) {
+        if (str.length == 0 || str[0].length() == haystack.length()) {
+            if (needle.equals("") || str.length == 0) {
                 return 0;
             } else {
                 return -1;
@@ -115,6 +116,47 @@ public class StringSet {
 
         return(int) res;
 
+    }
+
+    /**
+     * 有效的字母异位词
+     * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的一个字母异位词。
+     *
+     * 示例 1:
+     *
+     * 输入: s = "anagram", t = "nagaram"
+     * 输出: true
+     * 示例 2:
+     *
+     * 输入: s = "rat", t = "car"
+     * 输出: false
+     * 说明:
+     * 你可以假设字符串只包含小写字母。
+     *
+     * 进阶:
+     * 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        int[] countS = new int[26];
+        int[] countT = new int[26];
+        boolean flag = false;
+
+        if(s.length() != t.length()){
+            return flag;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            //统计每一个字母出现的次数
+            countS[s.charAt(i) - 'a']++;
+            countT[t.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(countS,countT)) {
+            flag = true;
+        }
+        return flag;
     }
 }
 
