ecca698de2c199d964d516c9af75f2b4cef1435f
wzz <568780720@qq.com>
2018-08-15 22:36:03 +0800

    leetcode  ·加一

--- src/StringSet.java
+++ src/StringSet.java
@@ -158,6 +158,36 @@ public class StringSet {
         }
         return flag;
     }
+
+    /**
+     * 加一
+     * 解法：更高位的进位由低位的进位而来，若低位无进位，则高位不用进位
+     * @param digits
+     * @return
+     */
+    public int[] plusOne(int[] digits) {
+        int carry =1;
+
+        //循环判断低位是否有进位
+        //若有进位，则继续计算；若无进位，则返回结果
+        for (int i = digits.length -1; i >= 0 ; i--) {
+            if(carry == 0){
+                return digits;
+            }
+            int temp = digits[i] + carry;
+            carry = temp / 10;
+            digits[i] = temp % 10;
+        }
+
+        //如果最高位有进位
+        if(carry == 1){
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            return result;
+        }
+
+        return digits;
+    }
 }
 
 
