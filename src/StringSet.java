1b71bf3f86aca6198477d1a1b5f74e052cf08eb1
wzz <568780720@qq.com>
2018-08-25 11:05:59 +0800

    leetcode   ·是不是3的幂

--- src/StringSet.java
+++ src/StringSet.java
@@ -532,7 +532,22 @@ public class StringSet {
      * 你能不使用循环或者递归来完成本题吗？
      */
     public boolean isPowerOfThree(int n) {
+        if(n <= 0){
+            return false;
+        }
 
+        if(n == 1 ){
+            return true;
+        }
+        if (n % 3 != 0 ){
+            return false;
+        }else if(n == 3 ){
+            return true;
+        }
+        else{
+            n = n/3 ;
+            return  isPowerOfThree(n);
+        }
     }
 }
 
