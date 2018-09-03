02bf87e63307b98598ed9a2b575a8f6759598e9a
wzz <568780720@qq.com>
2018-09-03 20:06:13 +0800

    相交链表

--- src/main/java/ListQ.java
+++ src/main/java/ListQ.java
@@ -461,6 +461,73 @@ public class ListQ {
 //
 //    }
 
+    /**
+     * 相交链表
+     * 编写一个程序，找到两个单链表相交的起始节点。
+     *
+     *
+     *
+     * 例如，下面的两个链表：
+     *
+     * A:          a1 → a2
+     *                    ↘
+     *                      c1 → c2 → c3
+     *                    ↗
+     * B:     b1 → b2 → b3
+     * 在节点 c1 开始相交。
+     *
+     *
+     *
+     * 注意：
+     *
+     * 如果两个链表没有交点，返回 null.
+     * 在返回结果后，两个链表仍须保持原有的结构。
+     * 可假定整个链表结构中没有循环。
+     * 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null)
+            return null;
+        ListNode aa = headA;
+        ListNode bb = headB;
+
+
+        while(headA != headB){
+               if(headA == null){
+                   headA = bb;
+               }else{
+                   headA = headA.next;
+               }
+               if(headB == null){
+                   headB = aa;
+               }else{
+                   headB = headB.next;
+               }
+            }
+            return headA;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**
      * 单向链表静态内部类
