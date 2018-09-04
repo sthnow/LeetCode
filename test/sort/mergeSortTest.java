649032838ba916360ef6359be861581bd37f2fa5
wzz <568780720@qq.com>
2018-09-04 23:58:24 +0800

    归并排序

---
+++ test/sort/mergeSortTest.java
@@ -0,0 +1,20 @@
+package sort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class mergeSortTest {
+@Before
+public void setUp(){
+    mergeSort ms = new mergeSort();
+    System.out.println(Arrays.toString(ms.sort(new int[]{1,7,5,4,3,8,9,1,2},0,8)));
+}
+
+    @Test
+    public void sort() {
+    }
+}
