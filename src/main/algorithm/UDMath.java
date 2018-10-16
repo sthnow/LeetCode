657dde46009e081d09f9eafd6448cdbf35660c32
wzz <568780720@qq.com>
2018-10-16 21:57:28 +0800

    x的平方根

---
+++ src/main/algorithm/UDMath.java
@@ -0,0 +1,45 @@
+public class UDMath {
+
+
+    /**
+     * x 的平方根
+     * 实现 int sqrt(int x) 函数。
+     * <p>
+     * 计算并返回 x 的平方根，其中 x 是非负整数。
+     * <p>
+     * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 4
+     * 输出: 2
+     * 示例 2:
+     * <p>
+     * 输入: 8
+     * 输出: 2
+     * 说明: 8 的平方根是 2.82842...,
+     * 由于返回类型是整数，小数部分将被舍去。
+     *
+     * @param x
+     * @return
+     */
+    public int mySqrt(int x) throws Exception {
+
+        if (x == 0)
+            return 0;
+
+        int left = 1, right = x, ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                ans = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+}
