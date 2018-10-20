edda7bf354de534411e91c8cdbcd5801a6150bab
sthnow <568780720@qq.com>
2018-10-20 20:51:23 +0800

    两数相除-leetcode

--- src/main/algorithm/MyMath.java
+++ src/main/algorithm/MyMath.java
@@ -95,6 +95,68 @@ public class MyMath {
 
 
     /**
+     * 两数相除
+     给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+
+     返回被除数 dividend 除以除数 divisor 得到的商。
+
+     示例 1:
+
+     输入: dividend = 10, divisor = 3
+     输出: 3
+     示例 2:
+
+     输入: dividend = 7, divisor = -3
+     输出: -2
+     说明:
+
+     被除数和除数均为 32 位有符号整数。
+     除数不为 0。
+     假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。本题中，如果除法结果溢出，则返回 231 − 1。
+     * @param dividend
+     * @param divisor
+     * @return
+     */
+    public int divide(int dividend, int divisor) {
+        // 相除时溢出处理
+        if (divisor == 0 || dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        // 求符号位
+        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+
+        // 求绝对值，为防止溢出使用long
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        // 记录结果
+        int result = 0;
+
+        // 被除数大于除数
+        while (dvd >= dvs) {
+            // 记录除数
+            long tmp = dvs;
+            // 记录商的大小
+            long mul = 1;
+
+            while (dvd >= (tmp << 1)) {
+                tmp <<= 1;
+                mul <<= 1;
+            }
+
+            // 减去最接近dvd的dvs的指数倍的值（值为tmp）
+            dvd -= tmp;
+
+            // 修正结果
+            result += mul;
+        }
+
+        return result * sign;
+    }
+
+
+    /**
      * 移位运算实现加减乘除详解以及java源码实现
      */
     /**
@@ -183,7 +245,7 @@ public class MyMath {
         boolean flag = (a < 0) ^ (b < 0);
         if (a < 0) a = -a;
         if (b < 0) b = -b;
-        if (a < b) return 0;
+
         int msb = 0;
 
         //找到 b 大于 a 时的比例
--- src/main/algorithm/MyMathTest.java
+++ src/main/algorithm/MyMathTest.java
@@ -5,8 +5,9 @@ public class MyMathTest {
 //        System.out.println( math.UDAdd(1, 19));
 //        System.out.println( math.UDSub(1,19));
 //        System.out.println(math.MyMuti(2,3));
-//        System.out.println(math.myDiv(6,2));
-        System.out.println(math.myDiv_v2(10,2));
+//      System.out.println(math.myDiv(2147483647,1));
+//        System.out.println(math.myDiv_v2(10,2));
+       System.out.println(math.divide(-2147483620,1));
     }
 
 }
