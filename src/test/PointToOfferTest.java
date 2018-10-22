8022fe7bfb08617bf800d216a51e319d5269d8bc
wzz <568780720@qq.com>
2018-10-22 22:30:37 +0800

    面试题3 二维数组中的查找

---
+++ src/test/PointToOfferTest.java
@@ -0,0 +1,6 @@
+public class PointToOfferTest {
+    public static void main(String[] args) {
+
+        System.out.println(PointToOffer.find(new int[][]{{1,2,8,9},{2,4,9,12},{4,7,10,12},{6,8,11,15}},4,4,10));
+    }
+}
