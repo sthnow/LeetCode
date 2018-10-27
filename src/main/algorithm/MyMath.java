42e22864a9f46f044c77b6e4309bdab97d03314b
wzz <568780720@qq.com>
2018-10-27 23:21:19 +0800

    Pow(x, n)

--- src/main/algorithm/MyMath.java
+++ src/main/algorithm/MyMath.java
@@ -303,5 +303,60 @@ public class MyMath {
         return quotient;
 
     }
+
+
+    /**
+     * Pow(x, n)
+     * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+     *
+     * 示例 1:
+     *
+     * 输入: 2.00000, 10
+     * 输出: 1024.00000
+     * 示例 2:
+     *
+     * 输入: 2.10000, 3
+     * 输出: 9.26100
+     * 示例 3:
+     *
+     * 输入: 2.00000, -2
+     * 输出: 0.25000
+     * 解释: 2-2 = 1/22 = 1/4 = 0.25
+     * 说明:
+     *
+     * -100.0 < x < 100.0
+     * n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
+     * @param x
+     * @param n
+     * @return
+     */
+    public double myPow(double x, int n) {
+        if(x == 1.0){
+            return x;
+        }
+
+
+        double sum = 1;
+        double temp = x;
+        boolean flag = false;
+        if(n < 0){
+            flag = true;
+            n = -n;
+        }
+        while(n != 0){
+            if(n % 2 == 1){
+                sum = sum * temp;
+            }
+
+            temp = temp * temp;
+            n = n >>> 1;
+        }
+        if(flag == false)
+            return sum;
+        else
+            return 1/sum;
+
+
+    }
     //类结束括号
 }
