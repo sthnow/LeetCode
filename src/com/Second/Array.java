de733c0b7468a2aa0f77d09f72ee5c9800fd0c3b
sthnow <568780720@qq.com>
2019-03-20 22:56:19 +0800

    只出现一次的数字

--- src/com/Second/Array.java
+++ src/com/Second/Array.java
@@ -212,6 +212,9 @@ public class Array {
      * @return
      */
     public boolean containsDuplicate(int[] nums) {
+//        思路：
+//          先对数组进行排序，那么重复的元素肯定是挨着的
+//          那么进行遍历，两两比较即可
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 1; i++){
             if(nums[i] == nums[i+1]){
@@ -221,4 +224,34 @@ public class Array {
         return false;
     }
 
+
+    /**
+     * 只出现一次的数字
+     * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+     *
+     * 说明：
+     *
+     * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+     *
+     * 示例 1:
+     *
+     * 输入: [2,2,1]
+     * 输出: 1
+     * 示例 2:
+     *
+     * 输入: [4,1,2,1,2]
+     * 输出: 4
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+//    思路：
+//        使用异或的特点，相异为1，相同为0，即一个数异或两个相同的数还是自己
+
+        int once = nums[0];
+        for(int i =1; i<nums.length; i++){
+            once = nums[i] ^ once;
+        }
+        return once;
+    }
 }
